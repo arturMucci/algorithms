78f0622b392ecfcd95729a6827da1ca0c0d86626
arturMucci <artur.mucci@gmail.com>
2023-06-01 16:59:04 -0300

    ProjetoAlgorithms - milestone 02

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,16 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("", "")
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(1, 1)
+
+    assert (
+        encrypt_message("pneumoultramicroscópicosilicovulcaniconiótico", 9)
+        == "tluomuenp_ocitóinocinacluvocilisocipócsorcimar"
+    )
+    assert encrypt_message("abcdef", 4) == "fe_dcba"
+    assert encrypt_message("abcdef", 10) == "fedcba"
